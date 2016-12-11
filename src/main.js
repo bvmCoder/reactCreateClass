6a28bf11a5d0e7d4a81be907574c33473feb24db
Dixit Patel <talkwithdixit@gmail.com>
2016-12-10 19:03:52 -0500

    added dev dependencies

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-$ = jQuery = require('jquery');
+var $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
